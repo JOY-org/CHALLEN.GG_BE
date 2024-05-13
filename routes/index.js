d5f7d08c03fd0a9fd0dced749e4a15ccde479443
SEOKWON <xylitolfrog@gmail.com>
2024-05-13 17:29:48 +0900

    update community

--- routes/index.js
+++ routes/index.js
@@ -32,8 +32,8 @@ router.use('/products',productRouter);
 router.get('/dummy', dummyInput)
 // 모두 같은 db를 쓰고 싶어서 -> dummy에 컨트롤러 dummy.js를 이용해서 모두가 /dummy 페이지들어가면 같은데이터를 갖고 시작할수 있다.
 
-// /v1/users
-router.use('/commynity', communityRouter);
+// /v1/community
+router.use('/community', communityRouter);
 
 
 
