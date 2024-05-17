50d3f2fc4be146fafafffba0ac2d4a684a7e1ffb
kwj7554 <99059160+kwj7554@users.noreply.github.com>
2024-05-17 10:13:34 +0900

    오류 수정

--- routes/auth.js
+++ routes/auth.js
@@ -11,15 +11,15 @@ router.post('/join', join);
 
 // // POST /v1/auth/login
 router.post('/auth/login', createToken);
+router.post('/auth/refresh', refreshToken);
 
 // /v1/auth/google
 router.get('/google',passport.authenticate('google'))
 router.get('/google/callback', googleLogin);
-router.post('/auth/refresh', refreshToken);
 
 // /v1/auth/kakao
 router.get('/kakao',passport.authenticate('kakao'))
 router.get('/kakao/callback', kakaoLogin);
-router.post('/auth/refresh', refreshToken);
+
 
 module.exports=router;
