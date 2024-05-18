111f38d19693e7c8b516ac3d36bc90168cf3a393
kwj7554 <99059160+kwj7554@users.noreply.github.com>
2024-05-18 14:01:39 +0900

    오류 수정

--- models/post.js
+++ models/post.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-class Posts extends Sequelize.Model{
+class Post extends Sequelize.Model{
     static initiate(sequelize){
-        Posts.init({
+        Post.init({
             img:{
                 type: Sequelize.STRING(225),
                 allowNull:false,
@@ -26,9 +26,9 @@ class Posts extends Sequelize.Model{
     }
     static associate(db){
         //관계들어갈 곳
-        db.Posts.belongsToMany(db.User,{foreignKey:"postid",through:'PostsLike'}) //Posts 테이블 생성
-        db.Posts.belongsTo(db.User)
-        db.Posts.belongsTo(db.Community)
+        db.Post.belongsToMany(db.User,{foreignKey:"postid",through:'PostsLike'}) //Posts 테이블 생성
+        db.Post.belongsTo(db.User)
+        db.Post.belongsTo(db.Community)
     }
 }
-module.exports=Posts;
+module.exports=Post;
