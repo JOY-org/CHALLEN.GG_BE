162bf0d6b1bce5be40ac3b581a5bca76f93a4b5b
kwj7554 <99059160+kwj7554@users.noreply.github.com>
2024-05-23 09:55:43 +0900

    이미지 한번에 보내기

--- controllers/post.js
+++ controllers/post.js
@@ -21,33 +21,54 @@ exports.getPost = async (req, res, next) => {
     }
 };
 
-exports.uploadPost = async(req,res,next)=>{
-    try{
-        const post = await Post.create({
-            title :req.body.title,
-            content:req.body.content,
-            category:req.body.category,
-            img:req.body.img,
-            UserId: req.user.id,
-        })
-        res.json({
-            code:200,
-            payload : post,
-            message:"업로드를 완료 했습니다.",
-            UserId:req.user.id,
-        })
-    }catch(err){
-        console.error(err);
-        next(err)
-    }
-}
+// exports.uploadPost = async(req,res,next)=>{
+//     try{
+//         const post = await Post.create({
+//             title :req.body.title,
+//             content:req.body.content,
+//             category:req.body.category,
+//             img:req.body.img,
+//             UserId: req.user.id,
+//         })
+//         res.json({
+//             code:200,
+//             payload : post,
+//             message:"업로드를 완료 했습니다.",
+//             UserId:req.user.id,
+//         })
+//     }catch(err){
+//         console.error(err);
+//         next(err)
+//     }
+// }
 
-exports.uploadImg =(req,res)=>{
-    res.json({
-        code: 200,
-        img:`/uploads/${req.file.filename}` //req.file.filename으로 받아 올수 있다.
-    })
-}
+// exports.uploadImg =(req,res)=>{
+//     res.json({
+//         code: 200,
+//         img:`/uploads/${req.file.filename}` //req.file.filename으로 받아 올수 있다.
+//     })
+// }
+
+exports.uploadPostAndImg = async (req, res, next) => {
+    try {
+        const post = await Post.create({
+            title: req.body.title,
+            content: req.body.content,
+            category: req.body.category,
+            img: req.file ? `/uploads/${req.file.filename}` : null,
+            UserId: req.user.id,
+        });
+
+        res.json({
+            code: 200,
+            payload: post,
+            message: "게시물과 이미지를 업로드했습니다.",
+        });
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+};
 
 exports.modifyPost = async (req, res, next) => {
     try {
