a87bc2a6fa7084cac56e149eb17f5708748ae16c
kwj7554 <99059160+kwj7554@users.noreply.github.com>
2024-05-24 11:32:38 +0900

    칼로리 조회

--- controllers/calorie.js
+++ controllers/calorie.js
@@ -6,6 +6,11 @@ exports.getCalorie = async(req,res,next) =>{
         const calorie =await Calorie.findAll({
             where:{id:req.user.id}
         })
+        res.json({
+            code:200,
+            payload:calorie,
+            message: "칼로리를 조회했습니다."
+        })
     } catch (err) {
         console.error(err);
         next(err)
